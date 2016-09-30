3278473407a68b9e4b872f4543b21197d3605e2c
GasparettoW <gasparetto.w@gmail.com>
2016-09-30 16:50:36 -0300

    commit
    footer and header done

--- public_html/apps/website/views/tpl/footer.php
+++ public_html/apps/website/views/tpl/footer.php
@@ -39,14 +39,15 @@
 
 
 					</div>
-					<div class="col-sm-5">
+					<div class="col-sm-5 mobile-hide redes">
 						<a href="https://www.facebook.com/CeluloseRS" target="_blank" >
 							<img src="<?=base_url('assets/img/logo-facebook.svg'); ?>">
 						</a>
-						<a href="" class="mobile-hide">
+						<a href="" >
 							<img src="<?=base_url('assets/img/logo-news.svg'); ?>">
 						</a>
 					</div>
+
 					<div class="col-sm-4 info">
 
 						Rua São Geraldo, 1680 <br>
@@ -58,14 +59,27 @@
 						<a href="http://www.conjunto.com.br/" target="_blank">( conjunto )</a>
 					</div>
 
-
-
-
 				</div>
 
 				<div class="border"></div>
 
 			</div>
 		</footer>
+
+		<script type="text/javascript" src="<?=base_url('assets/js/jquery.js'); ?>"></script>
+		<script type="text/javascript">var URL = {base: '<?=base_url(); ?>',site: '<?=site_url(); ?>',current: '<?=current_url(); ?>'};</script>
+		<script type="text/javascript" src="https://maps.google.com/maps/api/js?sensor=false&language=<?=getLangTerm(); ?>"></script>
+		<script type="text/javascript" src="<?=base_url('assets/js/lang/'.getLangTerm().'.js'); ?>"></script>
+		<script type="text/javascript" src="<?=base_url('assets/js/validate.js'); ?>"></script>
+		<script type="text/javascript" src="<?=base_url('assets/js/mask.js'); ?>"></script>
+		<script type="text/javascript" src="<?=base_url('assets/js/maps.js'); ?>"></script>
+		<script type="text/javascript" src="<?=base_url('assets/js/slider.js'); ?>"></script>
+
+		
+		<script type="text/javascript" src="<?=base_url('assets/js/main.js'); ?>"></script>
+		<script type="text/javascript" src="<?=base_url('assets/js/onload.js'); ?>"></script>
+		<script type="text/javascript" src="<?=base_url('assets/js/plugins/menu.js'); ?>"></script>
+		<script type="text/javascript" src="<?=base_url('assets/js/plugins/jquery.bxslider.js'); ?>"></script>
+
 	</body>
 	</html>
