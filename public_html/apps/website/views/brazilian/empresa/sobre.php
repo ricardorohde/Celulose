f6e1e41e7b87230b7e7fee5f4becd2741496b02e
GasparettoW <gasparetto.w@gmail.com>
2016-10-05 17:43:45 -0300

    sobre

--- public_html/apps/website/views/brazilian/empresa/sobre.php
+++ public_html/apps/website/views/brazilian/empresa/sobre.php
@@ -1,66 +1,96 @@
-<?php
-	$this->load->view(getLang().'/empresa/aba');
-?>
-<section class="container empresa">
-	<h1 class="title"><?=lang('defualt_menu_sobre'); ?></h1>
 
-	<img class="banner" alt="" src="<?=base_url('assets/img/banners/banner-empresa.jpg'); ?>">
 
-	<div class="descricao header">
-		A Celulose Riograndense, parte do grupo CMPC, é uma empresa gaúcha presente no mercado internacional de celulose de fibra curta de eucalipto. Ela conta com uma fábrica no município de Guaíba que ocupa hoje uma área de 106 hectares e investe no cultivo de florestas como fonte de suprimento de matéria-prima sustentável, a fim de produzir riquezas para o estado do Rio Grande do Sul e seus cidadãos. 
-		<br><br>
-		Para a Celulose Riograndense, crescer com os gaúchos é mais que uma prioridade, é um compromisso. Um compromisso com quem trabalha na empresa, com os cidadãos de Guaíba e com aqueles que fazem do solo riograndense um lugar melhor para viver.
+<img class="banner" alt="" src="<?=base_url('assets/img/banners/banner-empresa.jpg'); ?>">
+
+<section class="empresa">
+
+	<div class="container">
+		<div class="col-md-offset-1 col-md-10">
+
+
+			<h2><?=lang('defualt_menu_sobre'); ?></h2>
+
+			<p>A Celulose Riograndense, parte do grupo CMPC, é uma empresa gaúcha presente no mercado internacional de celulose de fibra curta de eucalipto. Ela conta com uma fábrica no município de Guaíba que ocupa hoje uma área de 106 hectares e investe no cultivo de florestas como fonte de suprimento de matéria-prima sustentável, a fim de produzir riquezas para o estado do Rio Grande do Sul e seus cidadãos. </p>
+
+			<p>Para a Celulose Riograndense, crescer com os gaúchos é mais que uma prioridade, é um compromisso. Um compromisso com quem trabalha na empresa, com os cidadãos de Guaíba e com aqueles que fazem do solo riograndense um lugar melhor para viver.</p>
+
+
+			<h3>Diferenciais</h3>
+
+			<p>	99,7% dos resíduos resultantes do processo de fabricação da celulose são reciclados.</p>
+
+
+			<h4>Avançado tratamento de efluentes</h4>
+			<p>
+				O que torna uma das poucas fábricas no mundo com esta estrutura.
+			</p>
+
+			<h4>Mínimo impacto</h4>
+			<p>
+				Na devolução do efluente tratado ao lago Guaíba.
+			</p>
+
+			<h4>Cogeração de energia</h4>
+			<p>
+				80% da energia necessária para produção da celulose é gerada pela própria fábrica.
+			</p>
+
+			<h3>Processos de Fabricação</h3>
+
+			<h4>Geração de Energia</h4>
+			<p>
+				A empresa gera energia para seu uso a partir de resíduos do processo de produção da celulose. Estes resíduos são queimados
+				em uma caldeira e o vapor produzido faz as turbinas funcionarem. Assim, temos vapor e energia elétrica para suprir as necessidades
+				da fábrica. A produção interna de energia poderia atender uma cidade de 200 mil habitantes.
+			</p>
+
+			<h4>Plantas Químicas</h4>
+			<p>
+				Boa parte dos produtos químicos utilizados pela Celulose Riograndense são produzidos internamente. O excedente é vendido ao mercado para os setores de tratamento de água, produtos de limpeza, plásticos, borrachas e setor alimentício.
+			</p>
+
+			<h4>Tratamento de Efluentes</h4>
+			<p>
+				A água para o processo de produção de celulose vem do Lago Guaíba. Depois de sua utilização, ela passa pelos tratamentos primário, secundário e terciário para ser novamente devolvida ao lago. A Celulose Riograndense foi pioneira na utilização desta tecnologia e, hoje, dentre as mais de mil fábricas de celulose presentes no mundo, menos de dez utilizam esta inovação, o que garante uma excelente qualidade ao efluente. O lodo gerado nesta planta passa por um processo de compostagem para ser, posteriormente, vendido ao mercado como fertilizante orgânico, substituindo o uso da terra preta retirada de banhados e matas.
+			</p>
+
+			<h4>Papel</h4>
+			<p>
+				A Celulose Riograndense dispõe de uma unidade de produção de papéis com capacidade de gerar 60 mil toneladas por ano, que atende um mercado diversificado nos segmentos de impressão e escrita.
+			</p>
+
+
+			<img alt="" src="<?=base_url('assets/img/'.getLang().'/esquema.jpg'); ?>"> 
+		</div>
 	</div>
+	<div class="clear"></div>
+</section>
+<section class="container empresa valores">
+	<h2 class="title"><?=lang('defualt_menu_valores'); ?></h2>
 
-	<h3 id="diferenciais">Diferenciais</h3>
+	<h4 class="header">Missão</h4>
 	<div class="descricao">
-		99,7% dos resíduos resultantes do processo de fabricação da celulose são reciclados.
+		Ofertar produtos obtidos de forma sustentável a partir de florestas plantadas, gerando benefícios econômicos, sociais e ambientais, contribuindo desta forma para o bem-estar e a qualidade de vida das pessoas.
 	</div>
 
-	<h4>Avançado tratamento de efluentes</h4>
+	<h4>Visão</h4>
 	<div class="descricao">
-		O que torna uma das poucas fábricas no mundo com esta estrutura.
+		Ser reconhecida como produtora mundial de celulose e papel, pela excelência na operação de seus processos e pelo respeito às suas partes interessadas.
 	</div>
 
-	<h4>Mínimo impacto</h4>
+	<h4>Política do Sistema de Gestão</h4>
 	<div class="descricao">
-		Na devolução do efluente tratado ao lago Guaíba.
+		A CMPC Celulose Riograndense, fornecedora no mercado mundial de celulose branqueada de eucalipto e de papel para impressão e escrita, considera que a qualidade de seus produtos e de seus serviços, providos por meio da operação e gestão sustentável de seu negócio, são fundamentais para assegurar retorno aos acionistas a partir de:
+		<ul>
+			<li>Fornecimento de produtos e serviços voltados às necessidades dos clientes;</li>
+			<li>Gestão orientada pela excelência operacional focada em resultados com melhoria contínua;</li>
+			<li>Uso sustentável dos recursos naturais e operações com impactos ambientais minimizados por meio de ações de prevenção e controle;</li>
+			<li>Atendimento à legislação, normas e compromissos assumidos formalmente pela empresa;</li>
+			<li>Relacionamento ético e comunicação transparente com as partes interessadas;</li>
+			<li>Promoção de um ambiente de trabalho motivador, com elevados padrões de saúde e segurança;</li>
+			<li>Pessoas capacitadas, motivadas e aptas a atuar eficazmente conforme as estratégias da empresa;</li>
+			<li>Desenvolvimento e aplicação de tecnologias que garantam inovação e competitividade.</li>
+		</ul>
 	</div>
-
-	<h4>Cogeração de energia</h4>
-	<div class="descricao">
-		80% da energia necessária para produção da celulose é gerada pela própria fábrica.
-	</div>
-
-	<h3 id="diferenciais" class="header">Processos de Fabricação</h3>
-
-	<h4>Geração de Energia</h4>
-	<div class="descricao">
-		A empresa gera energia para seu uso a partir de resíduos do processo de produção da celulose. Estes resíduos são queimados
-		<br>
-		em uma caldeira e o vapor produzido faz as turbinas funcionarem. Assim, temos vapor e energia elétrica para suprir as necessidades
-		<br>
-		da fábrica. A produção interna de energia poderia atender uma cidade de 200 mil habitantes.
-	</div>
-
-	<h4>Plantas Químicas</h4>
-	<div class="descricao">
-		Boa parte dos produtos químicos utilizados pela Celulose Riograndense são produzidos internamente. O excedente é vendido ao mercado para os setores de tratamento de água, produtos de limpeza, plásticos, borrachas e setor alimentício.
-	</div>
-
-	<h4>Tratamento de Efluentes</h4>
-	<div class="descricao">
-		A água para o processo de produção de celulose vem do Lago Guaíba. Depois de sua utilização, ela passa pelos tratamentos primário, secundário e terciário para ser novamente devolvida ao lago. A Celulose Riograndense foi pioneira na utilização desta tecnologia e, hoje, dentre as mais de mil fábricas de celulose presentes no mundo, menos de dez utilizam esta inovação, o que garante uma excelente qualidade ao efluente. O lodo gerado nesta planta passa por um processo de compostagem para ser, posteriormente, vendido ao mercado como fertilizante orgânico, substituindo o uso da terra preta retirada de banhados e matas.
-	</div>
-
-	<h4>Papel</h4>
-	<div class="descricao">
-		A Celulose Riograndense dispõe de uma unidade de produção de papéis com capacidade de gerar 60 mil toneladas por ano, que atende um mercado diversificado nos segmentos de impressão e escrita.
-	</div>
-
-	<br><br>
-
-	<img class="esquema" alt="" src="<?=base_url('assets/img/'.getLang().'/esquema.jpg'); ?>">
-
 	<div class="clear"></div>
 </section>
--- public_html/apps/website/views/brazilian/home.php
+++ public_html/apps/website/views/brazilian/home.php
@@ -25,39 +25,52 @@
 	<div class="container">
 		<div class="col-md-offset-1 col-md-10">
 			<div class="row">
+				
 				<div class="noticias">
 					<h2>Notícias</h2>
 
 					<?php
 					$i = 1;
-					foreach($noticias as $rows){
-						echo'<div class="col-xs-6 col-sm-3">
+					foreach ($noticias as $rows) {
+						if($i>4){
+							$auxClass = "mobile-hide";
+						}else{
+							$auxClass = "";
+						}
+
+						?>
+
+						<div class="col-xs-6 col-sm-3 <?= $auxClass ?>">
 							<div class="row">
-								<a href="',$rows['link'],'" class="item">
-									<img src="',$rows['img'],'" alt="',$rows['titulo'],'">
-								</a>
+								<div class="item">
+									<div class="content">
+										<a href="<?= $rows['link']; ?>">
+											<img src="<?= $rows['img']; ?>" alt="<?= $rows['titulo']; ?>">
+										</a>
+									</div>
+								</div>
 							</div>
-						</div>';						
-					}
-					?>
+						</div>
+						<?php $i++; } ?>
 
-				</div>
+						<span>Veja mais +</span>
+					</div>
 
-				<div class="projetos-sociais">
-					<h2>Projetos Sociais</h2>
+					<div class="projetos-sociais">
+						<h2>Projetos Sociais</h2>
 
-					<div class="col-xs-12">
-						<div class="home-projetos">
-							
+						<div class="col-xs-12">
+							<div class="home-projetos">
+
+							</div>
+						</div>
+						<div class="col-xs-12">
+
 						</div>
 					</div>
-					<div class="col-xs-12">
-						
-					</div>
+
 				</div>
-				
 			</div>
 		</div>
-	</div>
-</section>
+	</section>
 
